69e92c15824d9722287525087e6ee81a91918617
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-05-17 16:01:23 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import './container/Main'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootswatch/dist/sketchy/bootstrap.min.css"; 
+import Main from './container/Main';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header" >
+  
+
+<Main/>
+    
+   hello world
     </div>
   );
 }
