390e1d6c3c3d72324119bca22859cf6d5d8471b5
nk2303 <ngankhong03@gmail.com>
2020-05-22 16:19:07 -0700

    adding cass and modal for log in sign up

--- src/container/Account.js
+++ src/container/Account.js
@@ -2,75 +2,37 @@ import React, { useState } from 'react';
 import Login from '../component/Login';
 import SignUp from '../component/SignUp';
 import { Link } from 'react-router-dom';
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button';
-
+import Profile from './Profile';
+import Container from 'react-bootstrap/Container';
 
 
 const Account = () => {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const handleLogin = () => setShow(false);
-    const handleSignUp = () => setShow(false);
-    let user = undefined
+    
+    let user = "undefined"
     const list = () => {
         if (user) {
             return (
                 <div>
-                    <button className="btn btn-outline-primary">Signout </button>
+                    <Profile/>  
                     <Link className="btn btn-outline-primary" to="/homepage"> Homepage </Link>
                 </div >
             )
         }
         else {
             return (
-                // <div> {<Login />} {<SignUp />} </div>
                 <div>
-                    <div className="btn btn-outline-primary">
-                        <Button onClick={handleShow}>
-                            Login
-      </Button>
-
-                        <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>Login</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body><div>{<Login />}</div></Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="primary" onClick={handleLogin}>
-                                    Submit
-          </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
-                    <div className="btn btn-outline-primary">
-                        <Button onClick={handleShow}>
-                            SignUp
-      </Button>
-
-                        <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                                <Modal.Title>SignUp</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body><div>{<SignUp/>}</div></Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="primary" onClick={handleSignUp}>
-                                    Submit
-          </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
+                    <Login/>
+                    <SignUp/>
+                    <Link className="btn btn-outline-primary" to="/homepage"> Homepage </Link>
                 </div>
             )
         }
     }
     return (
-        <div>
+        <Container>
             Home Page Data
             {list()}
-        </div>
+        </Container>
     )
 }
 export default Account
