de77cbaed9c736b85d311f71c26d7dd2683241c7
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-05-21 17:32:16 -0700

    yeahooo

--- src/container/Account.js
+++ src/container/Account.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import Login from '../component/Login'
-import SignUp from '../component/SignUp'
+import React from "react";
+import Login from "../component/Login";
+import SignUp from "../component/SignUp";
 const Account = () => {
-    let user = undefined
-const list = () => {
-if (user) {
-return <div> 
-    {/* {<Signout/>}  {<Main/>} */}
-    </div>
-}
-else 
-{
-    return <div> {<Login/>} {<SignUp/>} </div>
-}
-}
-return (
-<div>Home Page Data{list()}</div>
-)
-}
-export default Account
+  let user = undefined;
+  const list = () => {
+    if (user) {
+      return <div>{/* {<Signout/>}  {<Main/>} */}</div>;
+    } else {
+      return (
+        <div>
+          {" "}
+          {<Login />} {<SignUp />}{" "}
+        </div>
+      );
+    }
+  };
+  return <div>Home Page Data{list()}</div>;
+};
+export default Account;
