d94436b4a53cec2b25480cd1dd1730c86c0c95c2
nk2303 <ngankhong03@gmail.com>
2020-05-23 21:07:36 -0700

    style

--- src/container/Account.js
+++ src/container/Account.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Login from '../component/Login';
 import SignUp from '../component/SignUp';
 import { Link } from 'react-router-dom';
--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SideBar from '../component/SideBar';
-import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
