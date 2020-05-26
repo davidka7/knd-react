26dba71eadeb5427cefd0cf29c34693518984d62
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-05-25 20:51:18 -0700

    update

--- src/component/Board.js
+++ src/component/Board.js
@@ -5,19 +5,6 @@ import Content from './Content';
 // import Modal from 'react-bootstrap/Modal';
 // import Button from 'react-bootstrap/Button';
 
-<<<<<<< HEAD
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-   
-return (
-<div>
-  <Card>
-    <Card.Body>
-      <Card.Title className="texter">Card title</Card.Title>
-      <Card.Text className="texter">
-   
-       <Button onClick={handleShow}>Card #####</Button>
-=======
 
 const Board = ({ board }) => {
 
@@ -31,7 +18,6 @@ const Board = ({ board }) => {
           
 
             {board.cards.map( card => <Content key={card.id} card={card}/>)
->>>>>>> f9dea2cbee9b8d3e4be2b20334a62cce0f610008
             
           }
 
