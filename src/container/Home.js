0dae52fafcc1d38ddf0aa4a40101ae591d654d5e
nk2303 <ngankhong03@gmail.com>
2020-06-10 13:41:32 -0700

    homepage deco again

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import './home.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+
 const Home = () => {
     
     return (
         <div className="home">
-
-            <img 
-                src={require(`../images/giphy.gif`)} 
-                alt={"giffy"} 
-                height="200px" width="200px" 
-                text-align="center"
-            />
-
-            <div class="jumbotron">
-                <h1 class="display-3">Welcome to Plany Boat!</h1>
-                <p class="lead">This is a simple app, created by two programing sailors.</p>
-                <hr class="my-4"></hr>
-                <p>You can create a project, Its boards with notes to keep track of your group or personal work.
-                    You also have the option to customize the app, and share your projects with your friends!
-                </p>
-            </div>
+            <Container className="jumbotron">
+                <Row>
+                    <Col xs={12} md={9}>
+                        <h1 className="display-3">Welcome to Plany Boat!</h1>
+                        <p className="lead">This is a simple app, created by two programing sailors: Kim and David.</p>
+                    </Col>
+                    <Col xs={12} md={3} className ="a" >
+                        <img src={require(`../images/giphy.gif`)} alt={"giffy"} height="200px" />
+                    </Col>
+                </Row>
+                <hr className="my-4"></hr>
+                <p className="size22">Plany Boat lets you work more collaboratively and get more done.</p>
+                <p className="size22">Boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.</p>
+            </Container>
 
         </div>
     )
