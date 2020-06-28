6587c2f9861aaeee186b670d527472feeed95566
nk2303 <ngankhong03@gmail.com>
2020-06-28 14:28:20 -0700

    improve view on mobile

--- src/container/Home.js
+++ src/container/Home.js
@@ -19,43 +19,45 @@ const Home = ({userInfo}) => {
                     <Col xs={12} md={9}>
                         <h1 className="display-3">Welcome to Plany Boat!</h1>
                         <br/>
-                        { userInfo ? <p className="text-info">Hi, {userInfo.username} !</p>
-                        :
-                        <> 
-                            <Row>
-                                <Col xs={12} md={5}>
-                                    <p className="lead">Have an account?</p>
-                                    <SignIn/>
-                                </Col>
-                                <Col xs={12} md={2}>
-                                    <p className="lead">Or</p>
-                                </Col>
-                                <Col xs={12} md={5}>
-                                    <p className="lead">Login with demo account</p>
-                                    <Demo/>
-                                </Col>
-                            </Row>
-                            <br/>
-                            <Row>
-                                <Col xs={12} md={5}>
-                                    <p className="lead">Don't have an account?</p>
-                                    <SignUp/>
-                                </Col>
-                                
-                            </Row>
-                        </>
-                        }
-                        <br/>
-                        <p className="lead">Plany Boat lets you work more collaboratively and get more done. <br/>
-                        Boards, lists, and cards enable you to organize and prioritize your projects in a fun and flexible way.</p>
+                        
                     </Col>
                     <Col xs={12} md={3} className ="a" >
                         <img src={require(`../images/giphy.gif`)} alt={"giffy"} height="200px" />
                     </Col>
                 </Row>
-                <hr className="my-4"></hr>
-                <p className="size22">This is a complex and interactive app, created by two programing sailors: Kim and David.</p>
-                <p className="size22"></p>
+                <Row>
+                    { userInfo ? <p className="text-info">Hi, {userInfo.username} !</p>
+                    :
+                    <> 
+                        <Row>
+                            <Col xs={12} md={5}>
+                                <p className="lead">Have an account?</p>
+                                <SignIn/>
+                            </Col>
+                            <Col xs={12} md={2}>
+                                <p className="lead center">Or</p>
+                            </Col>
+                            <Col xs={12} md={5}>
+                                <p className="lead">Login with demo account</p>
+                                <Demo/>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col xs={12} md={12}>
+                                <p className="lead">Don't have an account?</p>
+                                <SignUp/>
+                            </Col>
+                        </Row>
+                    </>
+                    }
+                    <br/><br/>
+                    <p className="lead">Plany Boat lets you work more collaboratively and get more done. <br/>
+                    Boards, lists, and cards enable you to organize and prioritize your projects in a fun and flexible way.</p>
+                    <hr className="my-4"></hr>
+                    <p className="size22">This is a complex and interactive app, created by two programing sailors: Kim and David.</p>
+                    <p className="size22"></p>
+                </Row>
             </Container>
 
         </div>
